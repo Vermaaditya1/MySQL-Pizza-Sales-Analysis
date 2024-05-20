4ef9fa771b6f24049aa5eac65b31875f0d84a4f0
Aditya Verma <vermaadityawork@gmail.com>
2024-05-20 20:18:09 +0530

    Add files via upload

---
+++ Codes/Table creation.sql
@@ -0,0 +1,18 @@
+create database pizza_analysis;
+use pizza_analysis;
+create table orders (
+order_id int not null,
+order_date date not null,
+order_time time not null,
+primary key(order_id)
+);
+
+drop table order_details;
+
+create table order_details (
+order_details_id int not null,
+order_id int not null,
+pizza_id text not null,
+quantity int not null,
+primary key(order_details_id)
+);
